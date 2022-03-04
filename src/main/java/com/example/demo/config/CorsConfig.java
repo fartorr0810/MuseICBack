b419c290bfd16e0e92b3b8dc3e7a5bd1b0a6efea
farrtor0810 <55539017+fartorr0810@users.noreply.github.com>
2022-03-04 08:20:23 +0100

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,55 +14,55 @@ public class CorsConfig implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/auth/register")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET", "POST","Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/auth/login")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET", "POST", "Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/user/**")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comentario")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET","POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/patinete")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/alquiler/**")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/calcular-alquiler")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/subida")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comprobar-email")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla")
+				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
