d687a2903468a1f790f33500e150864faad66236
Fran <frankarroyop@gmail.com>
2022-03-04 09:09:38 +0100

    e para emosion

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,58 +11,60 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			String RUTAEXTERNA="https://fartorr0810.github.io/MueveteSevilla/";
+			String RUTALOCAL="http://localhost:4200/";
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/auth/register")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET", "POST","Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/auth/login")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET", "POST", "Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/user/**")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comentario")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET","POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/patinete")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/alquiler/**")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/calcular-alquiler")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/subida")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comprobar-email")
-				.allowedOrigins("https://fartorr0810.github.io/MueveteSevilla/**")
+				.allowedOrigins(RUTALOCAL)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
