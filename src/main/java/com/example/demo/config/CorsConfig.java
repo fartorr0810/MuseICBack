f5828f671d0c6b712566193a0527ded0dbc6727e
Fran <frankarroyop@gmail.com>
2022-03-04 09:26:14 +0100

    Marroneria

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,60 +11,60 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
-			String RUTAEXTERNA="https://fartorr0810.github.io/MueveteSevilla/";
+			String RUTAEXTERNA="https://fartorr0810.github.io/";
 			String RUTALOCAL="http://localhost:4200/";
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/auth/register")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET", "POST","Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/auth/login")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET", "POST", "Content-Type","X-Requested-With",
 						"accept"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/user/**")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comentario")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET","POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/patinete")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/alquiler/**")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("GET", "POST", "PUT", "DELETE","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/calcular-alquiler")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/subida")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
 				.exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
 				registry.addMapping("/comprobar-email")
-				.allowedOrigins(RUTALOCAL)
+				.allowedOrigins(RUTAEXTERNA)
 				.allowedHeaders("POST","Content-Type","X-Requested-With",
 						"accept","Authorization"
 						,"Origin","Access-Control-Request-Method","Access-Control-Request-Headers")
